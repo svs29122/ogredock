fe9dd7caea3c4f90a26e9b0e446d81e2b1f63157
svs29122 <stephenstmichael@gmail.com>
2023-02-11 19:24:02 -0600

    Got containers appearing in HTML table using golang template engine

--- webmod/webmod.go
+++ webmod/webmod.go
@@ -3,34 +3,34 @@ package webmod
 import(
 	"fmt"
 	"net/http"
+	"html/template"
 	"ogredock/contmod"
 )
 
-type HelloHandler struct{}
-func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to OgreDock!")
+func hello (w http.ResponseWriter, r *http.Request){
+	t, _ := template.ParseFiles("./webmod/templates/tmpl.html")
+	t.Execute(w, "Welcome to OgreDock")
+	//fmt.Fprintf(w, "Welcome to OgreDock!")
 }
 
-type ContainerListHandler struct{}
-func (h *ContainerListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func listContainers (w http.ResponseWriter, r *http.Request){
 	containers := contmod.GetContainers()
 
-	for _, container := range containers{
-		fmt.Fprintf(w, "%s %s %s\n", container.ID[:10], container.Image, container.Names[0])
-	}
+	t, _ := template.ParseFiles("./webmod/templates/ctable.html")
+
+	t.Execute(w, containers)
 }
 
 func StartServer() {
 
-	hello := HelloHandler{}
-	clist := ContainerListHandler{}
+	fmt.Printf("starting server...")
 
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.Handle("/", &hello);
-	http.Handle("/list", &clist);
+	http.HandleFunc("/", hello);
+	http.HandleFunc("/list", listContainers);
 
 	server.ListenAndServe()
 }
