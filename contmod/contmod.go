eccd69c092793fa1ab4ec3c50ed773422cdde270
svs29122 <stephenstmichael@gmail.com>
2023-02-18 17:58:58 -0600

    able to create and start containers from web app, made todo list

--- contmod/contmod.go
+++ contmod/contmod.go
@@ -5,8 +5,55 @@ import (
 	"fmt"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
+
+type OgreDockConfig struct {
+	Client	*client.Client
+	HostCon	*container.HostConfig
+	NetCon	*network.NetworkingConfig
+}
+
+var OConfig OgreDockConfig;
+
+func initializeHostConfig(port string) *container.HostConfig{
+	newport, err := nat.NewPort("tcp", port)
+	if err != nil {
+		panic(err)
+	}
+
+	hostConfig := &container.HostConfig{
+			PortBindings: nat.PortMap{
+				newport: []nat.PortBinding{
+					{
+						HostIP: "0.0.0.0",
+						HostPort: port,
+					},
+				},
+			},
+			RestartPolicy: container.RestartPolicy{
+				Name: "always",
+			},
+	}
+
+	return hostConfig;
+}
+
+func initializeNetworkConfig(addr string , gateway string, net string) *network.NetworkingConfig{
+	networkConfig := &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{},
+	}
+	endpointConfig := &network.EndpointSettings{
+		Gateway: gateway,
+		IPAddress: addr,
+	}
+	networkConfig.EndpointsConfig[net] = endpointConfig
+
+	return networkConfig
+}
 
 func ListContainers(){
 	cli, err := client.NewClientWithOpts(client.FromEnv)
@@ -36,4 +83,36 @@ func GetContainers() []types.Container{
 	}
 
 	return containers
+}
+
+func CreateContainer(name string, net string, img string, ip string) (string, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	config := &container.Config{
+		Image: img,
+	}
+
+	cont, err2 := cli.ContainerCreate(context.Background(), config, nil, nil, nil, name)
+	if err2 != nil {
+		panic(err2)
+	}
+
+	return cont.ID, err2;
+}
+
+func RunContainer(contID string) error{
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	err2 := cli.ContainerStart(context.Background(), contID, types.ContainerStartOptions{})
+	if err2 != nil{
+		panic(err2)
+	}
+
+	return err;
 }
--- webmod/webmod.go
+++ webmod/webmod.go
@@ -7,30 +7,65 @@ import(
 	"ogredock/contmod"
 )
 
-func hello (w http.ResponseWriter, r *http.Request){
-	t, _ := template.ParseFiles("./webmod/templates/tmpl.html")
+func home (w http.ResponseWriter, r *http.Request){
+	t, _ := template.ParseFiles("./webmod/templates/index.html")
 	t.Execute(w, "Welcome to OgreDock")
+
+	//writes text to the Response Writer directly as opposed to using a template
 	//fmt.Fprintf(w, "Welcome to OgreDock!")
 }
 
-func listContainers (w http.ResponseWriter, r *http.Request){
+func containerManagement (w http.ResponseWriter, r *http.Request){
+	if r.Method == http.MethodPost {
+		r.ParseForm();
+
+		name := r.FormValue("Name")
+		net := r.FormValue("Net")
+		img := r.FormValue("Img")
+		cmds := []string{r.FormValue("start_button"), r.FormValue("stop_button"),r.FormValue("create_button"),r.FormValue("destroy_button")}
+
+		if cmds[0] != "" {
+			contId, err := contmod.CreateContainer(name, net, img, "");
+			if err != nil {
+				fmt.Printf("Create Container failed")
+			} else {
+				err = contmod.RunContainer(contId)
+				if err != nil {
+					fmt.Printf("Run Container failed")
+				} else {
+						fmt.Printf("%s %s %s %s\n", cmds[0], name, net, img);
+				}
+			}
+		} else if cmds[1] != "" {
+			fmt.Printf("%s %s %s %s\n", cmds[0], name, net, img);
+		} else if cmds[2] != "" {
+			_, err := contmod.CreateContainer(name, net, img, "");
+			if err != nil {
+				fmt.Printf("Create Container failed")
+			} else {
+				fmt.Printf("%s %s %s %s\n", cmds[0], name, net, img);
+			}
+		} else if cmds[3] != "" {
+			fmt.Printf("%s %s %s %s\n", cmds[0], name, net, img);
+		}
+	}
+
 	containers := contmod.GetContainers()
 
 	t, _ := template.ParseFiles("./webmod/templates/ctable.html")
-
 	t.Execute(w, containers)
 }
 
 func StartServer() {
 
-	fmt.Printf("starting server...")
+	fmt.Printf("starting server...\n")
 
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
-	http.HandleFunc("/", hello);
-	http.HandleFunc("/list", listContainers);
+	http.HandleFunc("/", home);
+	http.HandleFunc("/cmv", containerManagement);
 
 	server.ListenAndServe()
 }
