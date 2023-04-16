e8df8d2fe2bc7f04288de0544517263f76425c1d
svs29122 <stephenstmichael@yahoo.com>
2023-04-16 11:54:46 -0500

    Able to generate/terminate many containers at once

--- webmod/webmod.go
+++ webmod/webmod.go
@@ -2,6 +2,8 @@ package webmod
 
 import(
 	"fmt"
+	"strconv"
+
 	"net/http"
 	"html/template"
 	"ogredock/contmod"
@@ -9,10 +11,7 @@ import(
 
 func home (w http.ResponseWriter, r *http.Request){
 	t, _ := template.ParseFiles("./webmod/templates/index.html")
-	t.Execute(w, "Welcome to OgreDock")
-
-	//writes text to the Response Writer directly as opposed to using a template
-	//fmt.Fprintf(w, "Welcome to OgreDock!")
+	t.Execute(w, "Welcome to")
 }
 
 func containerManagement (w http.ResponseWriter, r *http.Request){
@@ -22,7 +21,10 @@ func containerManagement (w http.ResponseWriter, r *http.Request){
 		name := r.FormValue("Name")
 		net := r.FormValue("Net")
 		img := r.FormValue("Img")
-		cmds := []string{r.FormValue("start_button"), r.FormValue("stop_button"),r.FormValue("create_button"),r.FormValue("destroy_button")}
+		cmds := []string{r.FormValue("start_button"),
+								r.FormValue("stop_button"),
+								r.FormValue("create_button"),
+								r.FormValue("destroy_button")}
 
 		containers := contmod.GetContainers()
 		selectedContainers := []string{}
@@ -104,7 +106,9 @@ func networkManagement (w http.ResponseWriter, r *http.Request){
 		//fmt.Printf("%s\n %s %s %s\n %s %s %s\n", cmds, name, pif, drv, ipr, sn, gw)
 
 		if cmds[0] != "" {
-			contmod.CreateNetwork(name, pif, drv, ipr, sn, gw)
+			if name != "" && pif != "" && drv != "" && ipr != "" && sn != ""  && gw != "" {
+				contmod.CreateNetwork(name, pif, drv, ipr, sn, gw)
+			}
 		} else if cmds[1] != "" {
 			for i := 0; i < len(selectedNetworks); i++ {
 				err := contmod.DestroyNetwork(selectedNetworks[i])
@@ -122,16 +126,89 @@ func networkManagement (w http.ResponseWriter, r *http.Request){
 		}
 	}
 
+	//TODO: consider sorting this slice
 	networks := contmod.GetNetworks()
 
 	t, _ := template.ParseFiles("./webmod/templates/networks.html")
 	t.Execute(w, networks)
 }
 
-func bulkGeneration (w http.ResponseWriter, r *http.Request){
-	t, _ := template.ParseFiles("./webmod/templates/generate.html")
-	t.Execute(w, "Bulk Generation")
+func containerGeneration (w http.ResponseWriter, r *http.Request){
+	if r.Method == http.MethodPost {
+		r.ParseForm();
 
+		bname := r.FormValue("BName")
+		net := r.FormValue("Net")
+		img := r.FormValue("Img")
+		rlow := r.FormValue("RLow")
+		rhigh := r.FormValue("RHigh")
+		cmds := []string{ r.FormValue("generate_button"),
+							   r.FormValue("terminate_button")}
+
+		//fmt.Printf("%s %s %s %s\n", cmds, bname, net, img);
+
+		if cmds[0] != "" {
+			if bname != "" && net != "" && img != "" && rlow != "" && rhigh != "" {
+				//get the ranges
+				rangeLow, errLow := strconv.Atoi(rlow)
+				if errLow != nil {
+					panic(errLow)
+				}
+				rangeHigh, errHigh := strconv.Atoi(rhigh)
+				if errHigh != nil {
+					panic(errHigh)
+				}
+
+				//start the containers
+				for i := rangeLow; i <= rangeHigh; i++ {
+					name := bname + "-" + strconv.Itoa(i)
+					contId, err := contmod.CreateContainer(name, net, img, "");
+					err = contmod.StartContainer(contId)
+					if err != nil {
+						fmt.Println("Start Container failed)")
+					}
+				}
+			} else {
+					fmt.Println("Generate Container failed")
+			}
+		} else if cmds[1] != "" {
+			if bname != "" && rlow != "" && rhigh != "" {
+				//get the ranges
+				rangeLow, errLow := strconv.Atoi(rlow)
+				if errLow != nil {
+					panic(errLow)
+				}
+				rangeHigh, errHigh := strconv.Atoi(rhigh)
+				if errHigh != nil {
+					panic(errHigh)
+				}
+
+				destroyList := make(map[string]bool)
+				for i := rangeLow; i <= rangeHigh; i++ {
+					name := "/" + bname + "-" + strconv.Itoa(i)
+					destroyList[name] = true
+				}
+
+				//terminate the containers
+				containers := contmod.GetContainers()
+				for _, container := range containers {
+					if destroyList[container.Names[0]] {
+						errd := contmod.DestroyContainer(container.ID)
+						if errd != nil {
+							panic(errd)
+						}
+					}
+				}
+			} else {
+					fmt.Println("Terminate Container failed")
+			}
+		}
+	}
+
+	containers := contmod.GetContainers()
+
+	t, _ := template.ParseFiles("./webmod/templates/generate.html")
+	t.Execute(w, containers)
 }
 
 func StartServer() {
@@ -142,10 +219,13 @@ func StartServer() {
 		Addr: "127.0.0.1:8080",
 	}
 
+	fs := http.FileServer(http.Dir("webmod/public"))
+	http.Handle("/webmod/public/", http.StripPrefix("/webmod/public/", fs))
+
 	http.HandleFunc("/", home);
 	http.HandleFunc("/cmv", containerManagement);
 	http.HandleFunc("/nmv", networkManagement);
-	http.HandleFunc("/bgv", bulkGeneration);
+	http.HandleFunc("/cgv", containerGeneration);
 
 	server.ListenAndServe()
 }
